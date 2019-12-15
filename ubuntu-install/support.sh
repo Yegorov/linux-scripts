b78f35e7528e2674c555c653c55799bbc26d16d3
Artem Yegorov <yegorov0725@yandex.ru>
2019-12-15 22:40:27 +0300

    Add h264enc for support.sh

--- ubuntu-install/support.sh
+++ ubuntu-install/support.sh
@@ -2,9 +2,10 @@
 set -e
 
 echo "Install support packages"
+# https://ubuntu.fliplinux.com/h-264.html
+
 sudo apt-get update -y
 sudo apt-get install -y \
      gnome-tweak-tool \
-     gstreamer1.0-plugins-bad
-
-
+     gstreamer1.0-plugins-bad \
+     h264enc
