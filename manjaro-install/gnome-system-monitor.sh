e09e33a258e4c366adc9d869d123ff1e0a0569f3
Artem Yegorov <yegorov0725@yandex.ru>
2020-12-06 01:11:48 +0300

    Update manjaro-install

---
+++ manjaro-install/gnome-system-monitor.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Install gnome-system-monitor"
+# https://wiki.gnome.org/Apps/SystemMonitor
+# https://losst.ru/dispetcher-zadach-v-ubuntu
+# https://www.archlinux.org/packages/extra/x86_64/gnome-system-monitor/
+
+sudo pacman -Sy gnome-system-monitor
+
---
+++ manjaro-install/seahorse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Install seahorse"
+# https://wiki.gnome.org/Apps/Seahorse
+# http://ashep.org/2010/seahorse-obzor-ustanovka-sozdanie-klyuchej/
+
+sudo pacman -Sy seahorse
+
