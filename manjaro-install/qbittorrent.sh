0d6fc6f2969b337932486e49f3049c03b9965961
Artem Yegorov <yegorov0725@yandex.ru>
2021-03-08 14:29:56 +0300

    Add qbittorrent

---
+++ manjaro-install/qbittorrent.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Install qbittorrent"
+# https://www.qbittorrent.org/
+# https://losst.ru/luchshie-torrent-klienty-dlya-linux#1_qBittorrent
+
+sudo pacman -Sy qbittorrent
