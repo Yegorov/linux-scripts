c8f122c759be49b4027b3b51cb6034495a5b952d
Artem Yegorov <yegorov0725@yandex.ru>
2021-05-10 00:00:16 +0300

    Add linssid alacritty

---
+++ manjaro-install/alacritty.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+echo "Install alacritty"
+# https://archlinux.org/packages/community/x86_64/alacritty/
+# https://github.com/alacritty/alacritty
+
+sudo pacman -S alacritty
---
+++ manjaro-install/linssid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+echo "Install linssid"
+# https://archlinux.org/packages/community/x86_64/linssid/
+# https://www.unixmen.com/linssid-graphical-wi-fi-scanner-linux/
+
+sudo pacman -Sy linssid
+
